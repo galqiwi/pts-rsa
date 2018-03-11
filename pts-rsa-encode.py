be2b3553edc506e96c014cdb4b5480379f44a181
galqiwi <galqiwi@gmail.com>
2018-03-11 19:57:46 +0300

    rename

--- pts-rsa-encode.py
+++ pts-rsa-encode.py
@@ -16,8 +16,6 @@ n, e = [int(i, 16) for i in key.read().split(' ')]
 
 msg = input()
 
-print(int.from_bytes(str.encode(msg), byteorder='big'))
-
 msg_encoded = pow(int.from_bytes(msg.encode(), byteorder='big'), e, n)
 
 print(hex(msg_encoded), file=open('msg', 'w'))
--- pts-rsa-keygen.py
+++ pts-rsa-keygen.py
@@ -1,16 +1,13 @@
 def egcd(a, b):
     if a == 0:
-        return (b, 0, 1)
+        return (0, 1)
     else:
-        g, y, x = egcd(b % a, a)
-        return (g, x - (b // a) * y, y)
+        y, x = egcd(b % a, a)
+        return (x - (b // a) * y, y)
 
 def modinv(a, m):
-    g, x, y = egcd(a, m)
-    if g != 1:
-        raise Exception('modular inverse does not exist')
-    else:
-        return x % m
+    x, y = egcd(a, m)
+    return x % m
 
 def rsa_keygen(p, q):
 	n = p * q
